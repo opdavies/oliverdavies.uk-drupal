37589139d5eabfb9adae7d17b1a8512676b4ddb7
Oliver Davies <oliver@oliverdavies.dev>
2024-04-04 18:00:00 +0100

    Build Tailwind CSS within the theme

---
+++ web/themes/custom/opdavies/assets/tailwind.config.ts
@@ -0,0 +1,39 @@
+import colours from "tailwindcss/colors";
+import formsPlugin from '@tailwindcss/forms';
+import type { Config } from 'tailwindcss'
+import typographyPlugin from "@tailwindcss/typography";
+
+export default {
+  content: [
+    "./opdavies.theme",
+    "./templates/**/*.html.twig",
+  ],
+  theme: {
+    colors: {
+      black: "#000",
+      blue: {
+        primary: "#24608A",
+        400: "#60a5fa",
+      },
+      current: "currentColor",
+      grey: colours.stone,
+      inherit: "inherit",
+      transparent: "transparent",
+      white: "#fff",
+    },
+
+    extend: {
+      fontFamily: {
+        sans: [
+          "Roboto Condensed",
+          "Arial",
+          "Helvetica Neue",
+          "Helvetica",
+          "sans-serif",
+        ],
+      },
+    },
+  },
+  plugins: [formsPlugin, typographyPlugin],
+} satisfies Config
+
